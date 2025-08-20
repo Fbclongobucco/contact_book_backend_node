616a4bf119e72eeef021215f213a707901bf0156
longobuccodev <longobuccofbc@gmail.com>
2025-08-19 21:09:48 -0300

    add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
 
 
 async function bootstrap() {
@@ -10,6 +11,13 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true
   }))
+
+  app.use(helmet())
+
+  app.enableCors({
+    origin: "*"
+  })
+
   await app.listen(process.env.PORT ?? 3333);
 }
 bootstrap();
