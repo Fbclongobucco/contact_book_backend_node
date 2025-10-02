2dbce8d8a79b3c64f07ea9360b04eaad607f683f
longobucco <longobuccofbc@gmail.com>
2025-10-02 00:19:55 -0300

    fix cors configurarion

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,10 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: "http://localhost:3001", 
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
-    credentials: true,
-  });
+app.enableCors({
+  origin: true,       
+  credentials: true,  
+});
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
